d913b1a1ff78e5fff50abea303856ac8bf0167cd
Andres Felipe Escobar Cardona <57599404+AndresEscobarDev@users.noreply.github.com>
2022-05-08 19:00:48 -0500

    USB Discount setup and endpoints

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const product = require('./routes/product.routes')
 const image = require('./routes/image.routes')
 const quote = require('./routes/quote.routes')
 const discount = require('./routes/discount.routes')
+const usbdiscount = require('./routes/usbDiscount.routes')
 
 const routes = (app) => {
   app.use('/user', user)
@@ -14,6 +15,7 @@ const routes = (app) => {
   app.use('/image', image)
   app.use('/quote', quote)
   app.use('/discount', discount)
+  app.use('/usbdiscount', usbdiscount)
 }
 
 module.exports = routes;
